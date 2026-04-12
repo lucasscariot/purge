84639133cbd8ff6fca4b81f9543992ab94b21431
Lucas Scariot <lucas@scariot.fr>
2026-04-12 09:43:59 +0200

    Initial Commit

---
+++ Purge/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Purge
+//
+//  Created by Lucas Scariot on 12/04/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Purge/PurgeApp.swift
@@ -0,0 +1,32 @@
+//
+//  PurgeApp.swift
+//  Purge
+//
+//  Created by Lucas Scariot on 12/04/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PurgeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
