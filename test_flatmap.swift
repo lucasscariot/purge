c6ef46eddbe102c5625de06aa7e15b2758a19dab
Lucas Scariot <lucas@scariot.fr>
2026-04-13 17:50:52 +0200

    chore: remove obsolete patch and test files

--- test_flatmap.swift
+++
@@ -1,6 +0,0 @@
-let a = [["a", "b"], ["c", "d"]]
-let b = [["e", "f"]]
-struct Obj { let sets: [[String]] }
-let objs = [Obj(sets: a), Obj(sets: b)]
-let result = objs.flatMap { $0.sets }
-print(result)
